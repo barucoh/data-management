48739357e896dbfd0b821db2078f3d53d223384b
Ohad Baruch <ohadbaruch1@gmail.com>
2017-12-09 21:33:14 +0200

    Added class picture and mongoDB

---
+++ DataManagement/DataManagement/Models/DataAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DataManagement.Models
+{
+    public class DataAccess
+    {
+        MongoClient _client;
+        MongoServerAddress _server;
+        IMongoDatabase _db;
+
+        public DataAccess()
+        {
+            _client = new MongoClient("mongodb://localhost:27017");
+            _server = _client.Settings.Server;
+            _db = _client.GetDatabase("EmployeeDB");
+        }
+    }
+}
